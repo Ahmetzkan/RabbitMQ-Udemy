1086014210666d22ac9e26802d2d5fe2e16e531c
Ahmet Özkan <ahmet.ozkan98@hotmail.com>
2025-03-02 01:37:52 +0300

    Add project files.

---
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+await using var connection = await factory.CreateConnectionAsync();
+
+await using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync("hello-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+string message = "Hello RabbitMQ";
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(string.Empty, "hello-queue", body: messageBody.AsMemory());
+
+Console.WriteLine("Message is sent");
+Console.ReadLine();
---
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+await using var connection = await factory.CreateConnectionAsync();
+
+await using var channel = await connection.CreateChannelAsync();
+
+//await channel.QueueDeclareAsync("hello-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+channel.BasicConsumeAsync("hello-queue", true, consumer);
+
+consumer.ReceivedAsync += (object sender, BasicDeliverEventArgs @event) =>
+{
+    var message = Encoding.UTF8.GetString(@event.Body.ToArray());
+    Console.WriteLine("Message: " + message);
+    return Task.CompletedTask;
+};
+
+Console.ReadLine();
